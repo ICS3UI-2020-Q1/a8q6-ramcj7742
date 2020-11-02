a24556f1efc3ca7f3a8e944b38e6970c50a09ad9
ramcj7742 <ramcj7742@wrdsb.ca>
2020-11-02 14:07:23 +0000

    Save 1

--- Main.java
+++ Main.java
@@ -5,7 +5,22 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+  JPanel topPanel;
+  JPanel buttonPanel;
+
+  JTextField display;
+  String displayText;
+
+  JButton[] numberButtons;
+  JButton enter;
+  JButton clear;
+
+  Font biggerFont;
+
+  int astericksAmount;
+
+  int[] astericksArray;
 
 
   // Method to assemble our GUI
@@ -18,6 +33,78 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    //initialize the main panel with a border layout
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+
+    // initialize the button panel with grid layout
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(4, 3));
+
+    //initialize the top panel with defualt layout (flow)
+    topPanel = new JPanel();
+
+    //initialize bigger Font
+    biggerFont = new Font("arial", Font.PLAIN, 24);
+    //create the display
+    display = new JTextField("CLOSE");
+    //set a preferred size
+    display.setPreferredSize(new Dimension(800, 100));
+    //add to top
+    topPanel.add(display);
+    //creates bigger font for display
+    display.setFont(biggerFont);
+
+    //creating the number buttons
+    numberButtons = new JButton[10];
+    for(int i = 0; i < numberButtons.length; i++){
+      numberButtons[i] = new JButton("" + i);
+      //set the action command
+      numberButtons[i].setActionCommand("" + i);
+      //add action ActionListener
+      numberButtons[i].addActionListener(this);
+    }
+
+    //initialize the array to store input
+    astericksArray = new int[4];
+
+    //create enter button
+    enter = new JButton("Enter");
+    enter.addActionListener(this);
+    enter.setActionCommand("enter");
+
+    //create clear button
+    clear = new JButton("CLEAR");
+    clear.addActionListener(this);
+    clear.setActionCommand("clear");
+
+    //initialize amount of astericks Variable
+    astericksAmount = 0;
+
+    //add buttons to grid
+    buttonPanel.add(numberButtons[1]);
+    buttonPanel.add(numberButtons[2]);
+    buttonPanel.add(numberButtons[3]);
+
+    buttonPanel.add(numberButtons[4]);
+    buttonPanel.add(numberButtons[5]);
+    buttonPanel.add(numberButtons[6]);
+
+    buttonPanel.add(numberButtons[7]);
+    buttonPanel.add(numberButtons[8]);
+    buttonPanel.add(numberButtons[9]);
+
+    buttonPanel.add(clear);
+    buttonPanel.add(numberButtons[0]);
+    buttonPanel.add(enter);
+
+    //assemble the panels
+    mainPanel.add(topPanel, BorderLayout.PAGE_START);
+    mainPanel.add(buttonPanel, BorderLayout.CENTER);
+
+    //add the main panel to the frame
+    frame.add(mainPanel);
  
     
 
@@ -27,6 +114,75 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    displayText = display.getText();
+    if(displayText == ("CLOSE")){
+      display.setText("");
+    }
+
+    //checks if code entered is correct
+    if(command.equals("enter") && astericksArray[0] == 1 && astericksArray[1] == 2 && astericksArray[2] == 3 && astericksArray[3] == 4){
+      display.setText("OPEN");
+    }else if(command.equals("enter")){
+      display.setText("WRONG PIN");
+    }
+
+    //detects the number the user entered, saves it, and displays an astericks
+    if(command.equals("1")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 1;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("2")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 2;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("3")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 3;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("4")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 4;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("5")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 5;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("6")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 6;astericksAmount = astericksAmount + 1;
+    }else if(command.equals("7")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 7;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("8")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 8;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("9")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 9;
+      astericksAmount = astericksAmount + 1;
+    }else if(command.equals("0")){
+      displayText = display.getText();
+      display.setText(displayText + "*");
+      astericksArray[astericksAmount] = 0;
+      astericksAmount = astericksAmount + 1;
+    }
+
+    //clears all data
+    if(command.equals("clear")){
+      run();
+    }
 
   }
 
